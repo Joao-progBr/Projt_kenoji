0b14e4bd83f1dae476248631b939d60cd6b548a5
joãoA <joaovictor.dudu03@gmail.com>
2025-05-09 00:45:30 -0300

    utilizando commonJs

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,93 @@
-// const gulp = require('gulp')
-// const cleanCss = require('gulp-clean-css')
-// const less = require('gulp-less')
-// const rename = require('gulp-rename')
-// const replace = require('gulp-replace')
-// const imagemin = require('gulp-imagemin')
-
-import gulp from 'gulp';
-import cleanCss from 'gulp-clean-css';
-import less from 'gulp-less';
-import rename from 'gulp-rename';
-import replace from 'gulp-replace';
-import imagemin from 'gulp-imagemin';
-// import uglify from 'gulp-uglify'
-import terser from 'gulp-terser'
-
-import mozjpeg from 'imagemin-mozjpeg';
-import pngquant from 'imagemin-pngquant';
-import gifsicle from 'imagemin-gifsicle';
-import svgo from 'imagemin-svgo';
+const gulp = require('gulp');
+const cleanCss = require('gulp-clean-css');
+const less = require('gulp-less');
+const rename = require('gulp-rename');
+const replace = require('gulp-replace');
+const imagemin = require('gulp-imagemin');
+const terser = require('gulp-terser')
 
 
-
-function styles(){
-  return gulp.src('./src/styles/*less')
-  .pipe(less())
-  .pipe(cleanCss())
-  .pipe(rename({suffix: '.min'}))
-  .pipe(gulp.dest('./dist/styles'))
+function styles() {
+  return gulp.src('./src/styles/*.less')
+    .pipe(less())
+    .pipe(cleanCss())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./dist/styles'));
 }
 
-function html(){
+function html() {
   return gulp.src('./src/index.html')
-  .pipe(replace(`./styles/style.less`, './styles/style.min.css'))
-  .pipe(gulp.dest('./dist'))
+    .pipe(replace('./styles/style.less', './styles/style.min.css'))
+    .pipe(gulp.dest('./dist'));
 }
 
-function script(){
+
+function script() {
   return gulp.src('./src/scripts/*.js')
-  .pipe(terser())
-  .pipe(gulp.dest('./dist/scripts'))
-
+    .pipe(terser())
+    .pipe(gulp.dest('./dist/scripts'));
 }
+
+function images(){
+  return gulp.src('./src/images/**/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('./dist/images'))
+}
+
+
+exports.default = gulp.parallel(styles, html, images, script)
+exports.watch = function(){
+  gulp.watch ('./src/styles/*.less',gulp.parallel(styles))
+  gulp.watch ('./src/scripts/*.js',gulp.parallel(script))
+  gulp.watch ('./src/index.html',gulp.parallel(html))
+  gulp.watch ('./src/images/**/*',gulp.parallel(images))
+}
+
+
+
+
+
+
+
+
+
+
+// import gulp from 'gulp';
+// import cleanCss from 'gulp-clean-css';
+// import less from 'gulp-less';
+// import rename from 'gulp-rename';
+// import replace from 'gulp-replace';
+// import imagemin from 'gulp-imagemin';
+// import uglify from 'gulp-uglify'
+// import terser from 'gulp-terser'
+
+// import mozjpeg from 'imagemin-mozjpeg';
+// import pngquant from 'imagemin-pngquant';
+// import gifsicle from 'imagemin-gifsicle';
+// import svgo from 'imagemin-svgo';
+
+
+
+// export function styles(){
+//   return gulp.src('./src/styles/*less')
+//   .pipe(less())
+//   .pipe(cleanCss())
+//   .pipe(rename({suffix: '.min'}))
+//   .pipe(gulp.dest('./dist/styles'))
+// }
+
+// export function html(){
+//   return gulp.src('./src/index.html')
+//   .pipe(replace(`./styles/style.less`, './styles/style.min.css'))
+//   .pipe(gulp.dest('./dist'))
+// }
+
+// export function script(){
+//   return gulp.src('./src/scripts/*.js')
+//   .pipe(terser())
+//   .pipe(gulp.dest('./dist/scripts'))
+
+// }
 
 
 // function images(){
@@ -49,26 +96,26 @@ function script(){
 //     .pipe(gulp.dest('./dist/images'))
 // }
 
-export function images() {
-  return gulp.src('./src/images/**/*')
-    .pipe(imagemin([
-        gifsicle({ interlaced: true }),
-        mozjpeg({ quality: 75, progressive: true }),
-        pngquant({ quality: [0.6, 0.8] }),
-        svgo()
-    ]))
-    .pipe(gulp.dest('./dist/images'));
-}
+// export function images() {
+//   return gulp.src('./src/images/**/*')
+//     .pipe(imagemin([
+//         gifsicle({ interlaced: true }),
+//         mozjpeg({ quality: 75, progressive: true }),
+//         pngquant({ quality: [0.6, 0.8] }),
+//         svgo()
+//     ]))
+//     .pipe(gulp.dest('./dist/images'));
+// }
 
-export const build = gulp.parallel(styles, html, images, script)
-export const watch = function(){
-    gulp.watch('./src/styles/*.less', gulp.parallel(styles))
-    gulp.watch('./src/index.html', gulp.parallel(html))
-    gulp.watch('./src/images/**/*', gulp.parallel(images));
-    gulp.watch('./src/scripts/*.js', gulp.parallel(script));
-}
+// export const build = gulp.parallel(styles, html, images, script)
+// export const watch = function(){
+//     gulp.watch('./src/styles/*.less', gulp.parallel(styles))
+//     gulp.watch('./src/index.html', gulp.parallel(html))
+//     gulp.watch('./src/images/**/*', gulp.parallel(images));
+//     gulp.watch('./src/scripts/*.js', gulp.parallel(script));
+// }
 
-export default build;
+// export default build;
 
 
 
